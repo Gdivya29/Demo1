eee1288f545dae121354453c27c1085c33ddd095
Gdivyata29 <divyatagarje1@gmail.com>
2022-11-12 17:03:13 +0530

    add files

---
+++ config.py
@@ -0,0 +1,13 @@
+import os 
+
+PORT = 8080
+
+# MODEL_FILE_PATH = r'/Users/rockys/Desktop/Trial/Iris_Web_Framework/LOM_IRIS_SPECIES/Project_app/LogisticModelIris.pkl'
+# JSON_FILE_PATH = r'/Users/rockys/Desktop/Trial/Iris_Web_Framework/LOM_IRIS_SPECIES/Project_app/Project_data.json'
+
+MODEL_FILE_PATH = os.path.join('Project_app','LogisticModelIris.pkl')
+JSON_FILE_PATH = os.path.join('Project_app','Project_data.json')
+
+
+# os.path.join('Project_app','Project_data')
+# os.path.join('Project_app','LinearModelIris')
